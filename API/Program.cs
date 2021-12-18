6e488da207da1bc243daedee56cc33a52aad441d
nagineni1 <hari4chinni@gmail.com>
2021-12-18 19:29:38 +0530

    Authentication completed

--- API/Program.cs
+++ API/Program.cs
@@ -1,8 +1,11 @@
 using API.ExceptionMiddleware;
 using API.Extensions;
 using API.Helpers;
+using Core.Entities.Identity;
 using Infrastructure.Data;
+using Infrastructure.Identity;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi.Models;
 using StackExchange.Redis;
@@ -18,6 +21,7 @@ builder.Services.AddEndpointsApiExplorer();
 
 var config = builder.Services.BuildServiceProvider().GetRequiredService<IConfiguration>();
 builder.Services.AddDbContext<StoreContext>(x => x.UseSqlServer(config.GetConnectionString("DefaultConnection")));
+builder.Services.AddDbContext<AppIdentityDbContext>(x => x.UseSqlServer(config.GetConnectionString("IdentityConnection")));
 builder.Services.AddSingleton<IConnectionMultiplexer>(c =>
 {
     var configuration = ConfigurationOptions.Parse(config.GetConnectionString("Redis"), true);
@@ -25,6 +29,7 @@ builder.Services.AddSingleton<IConnectionMultiplexer>(c =>
 });
 builder.Services.AddAutoMapper(typeof(MappingProfiles));
 builder.Services.AddApplicationServices();
+builder.Services.AddIdentityServices(config);
 builder.Services.AddSwaggerDocumentation();
 var loggerFactory = builder.Services.BuildServiceProvider().GetRequiredService<ILoggerFactory>();
 
@@ -33,12 +38,17 @@ var loggerFactory = builder.Services.BuildServiceProvider().GetRequiredService<I
         var context = builder.Services.BuildServiceProvider().GetRequiredService<StoreContext>();
         await context.Database.MigrateAsync();
         await StoreContextSeed.SeedAsync(context, loggerFactory);
+        var userManager = builder.Services.BuildServiceProvider().GetRequiredService<UserManager<AppUser>>();
+        var identityContext = builder.Services.BuildServiceProvider().GetRequiredService<AppIdentityDbContext>();
+        await identityContext.Database.MigrateAsync();
+        await AppIdentityDbContextSeed.SeedUsersAsync(userManager);
     }
     catch (Exception ex)
     {
         var logger = loggerFactory.CreateLogger<Program>();
         logger.LogError(ex, "An error occured while migration");
     }
+
 builder.Services.AddCors(opt =>
 {
     opt.AddPolicy("CorsPolicy", policy =>
@@ -57,6 +67,7 @@ app.UseHttpsRedirection();
 app.UseRouting();
 app.UseStaticFiles();
 app.UseCors("CorsPolicy");
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
---
+++ Infrastructure/Identity/AppIdentityDbContextSeed.cs
@@ -0,0 +1,36 @@
+﻿using Core.Entities.Identity;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Identity
+{
+    public class AppIdentityDbContextSeed
+    {
+        public static async Task SeedUsersAsync(UserManager<AppUser> userManager)
+        {
+            if (!userManager.Users.Any())
+            {
+                var user = new AppUser
+                {
+                    DisplayName = "Bob",
+                    Email = "bob@test.com",
+                    UserName = "bob@test.com",
+                    Address = new Address
+                    {
+                        FirstName = "Bob",
+                        LastName = "Bobbity",
+                        City = "Nellore",
+                        Street = "10 the street",
+                        State = "AP",
+                        ZipCode = "524343"
+                    }
+                };
+                await userManager.CreateAsync(user,"Pa$$w0rd");
+            }
+        }
+    }
+}
